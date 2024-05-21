d454289ca6e6457277fe1b9a04e0c5b829c57954
Kaif-Shariff <kaif.shariff1234@gmail.com>
2024-05-21 22:20:54 +0530

    Build apk | Add: category, sub-category, product data model

--- lib/widget/category_widget.dart
+++ lib/widget/category_widget.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:speedy_delivery/screens/categories_screen.dart';
