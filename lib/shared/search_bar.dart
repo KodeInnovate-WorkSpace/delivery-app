b542da0d2b224ed330710aab470f1df5527b6b4e
SK Moinuddin <98759447+Skmoin5502@users.noreply.github.com>
2024-05-29 10:20:00 +0530

    search_bar.dart

--- lib/shared/search_bar.dart
+++ lib/shared/search_bar.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:speedy_delivery/screens/search_functionality.dart';
 
-Widget searchBar() {
-  return TextField(
-    cursorColor: Colors.black,
-    decoration: InputDecoration(
-      hintText: 'Search for \'product\'',
-      hintStyle: const TextStyle(color: Colors.grey),
-      filled: true,
-      fillColor: Colors.white,
-      enabledBorder: OutlineInputBorder(
-        borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-        borderSide: BorderSide(color: Colors.grey.shade300),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-        borderSide: BorderSide(color: Colors.grey[300]!),
-      ),
-      contentPadding: const EdgeInsets.symmetric(horizontal: 15),
-      prefixIcon: const Icon(
-        Icons.search,
+Widget searchBar(BuildContext context) {
+  return GestureDetector(
+    onTap: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => SearchPage()), // Replace SearchScreen with your desired screen
+      );
+    },
+    child: AbsorbPointer(
+      child: TextField(
+        cursorColor: Colors.black,
+        decoration: InputDecoration(
+          hintText: 'Search for \'product\'',
+          hintStyle: const TextStyle(color: Colors.grey),
+          filled: true,
+          fillColor: Colors.white,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+            borderSide: BorderSide(color: Colors.grey.shade300),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+            borderSide: BorderSide(color: Colors.grey[300]!),
+          ),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 15),
+          prefixIcon: const Icon(
+            Icons.search,
+          ),
+        ),
+        style: const TextStyle(color: Colors.black),
       ),
     ),
-    style: const TextStyle(color: Colors.black),
   );
 }
