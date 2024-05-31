d69b001a4ad528dee3e0753d3df5a00839ea3be5
Kaif-Shariff <kaif.shariff1234@gmail.com>
2024-05-31 10:06:50 +0530

    Add: Search function

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:speedy_delivery/firebase_options.dart';
@@ -10,6 +11,7 @@ import 'package:speedy_delivery/providers/location_provider.dart';
 import 'package:speedy_delivery/providers/product_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:speedy_delivery/screens/home_screen.dart';
+import 'package:speedy_delivery/screens/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,8 +30,37 @@ Future<void> main() async {
   ], child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isLogin = false;
+
+  @override
+  void initState() {
+    super.initState();
+    checkIfLogin(); // Call this method when the app starts
+  }
+
+  Future<void> checkIfLogin() async {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user != null && mounted) {
+        // User is logged in
+        setState(() {
+          isLogin = true;
+        });
+      } else {
+        // User is not logged in
+        setState(() {
+          isLogin = false;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,8 +79,8 @@ class MyApp extends StatelessWidget {
         ),
       ),
       // home: const SampleScreen(),
-      home: const HomeScreen(),
-      // home: const SplashScreen(),
+      // home: const HomeScreen(),
+      home: isLogin ? const HomeScreen() : const SplashScreen(),
     );
   }
 }
--- lib/screens/search_functionality.dart
+++ lib/screens/search_functionality.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../widget/add_to_cart_button.dart';
 
 class Product {
   final String name;
   final String imageUrl;
+  final double price;
 
-  Product({required this.name, required this.imageUrl});
+  Product({required this.name, required this.imageUrl, required this.price});
 }
 
-class SearchPage extends StatefulWidget {
-  const SearchPage({super.key});
 
+class SearchPage extends StatefulWidget {
   @override
   _SearchPageState createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
-  final TextEditingController _controller = TextEditingController();
+  TextEditingController _controller = TextEditingController();
   List<Product> _allProducts = [];
   List<Product> _filteredProducts = [];
   List<Product> _recentSearches = [];
+  List<Product> _productSearches = []; // Renamed from _sessionSearches
+  Map<String, int> _productCounts = {}; // Add this line
 
   @override
   void initState() {
@@ -29,13 +32,16 @@ class _SearchPageState extends State<SearchPage> {
   }
 
   Future<void> fetchProductsFromFirestore() async {
-    final productsCollection =
-        FirebaseFirestore.instance.collection('products');
+    final productsCollection = FirebaseFirestore.instance.collection(
+        'products');
     final snapshot = await productsCollection.get();
     final products = snapshot.docs.map((doc) {
       return Product(
         name: doc['name'] as String,
         imageUrl: doc['image'] as String,
+        price: doc['price'] is int
+            ? (doc['price'] as int).toDouble()
+            : doc['price'] as double, // Handle price as int or double
       );
     }).toList();
 
@@ -43,6 +49,7 @@ class _SearchPageState extends State<SearchPage> {
       _allProducts = products;
     });
   }
+
 
   void filterProducts() {
     final query = _controller.text.toLowerCase();
@@ -57,16 +64,19 @@ class _SearchPageState extends State<SearchPage> {
     });
   }
 
+
   void saveSearch(Product product) {
     setState(() {
+      _productSearches.clear(); // Renamed from _sessionSearches
+      _productSearches.add(product); // Renamed from _sessionSearches
       _recentSearches.removeWhere((p) =>
-          p.name ==
-          product.name); // Remove if already exists to avoid duplicates
+      p.name == product.name); // Remove if already exists to avoid duplicates
       _recentSearches.insert(0, product); // Insert at the beginning
       if (_recentSearches.length > 5) {
         _recentSearches =
             _recentSearches.sublist(0, 5); // Keep only the last 5 searches
       }
+      _productCounts[product.name] = 0; // Initialize the count to 0
     });
   }
 
@@ -109,6 +119,103 @@ class _SearchPageState extends State<SearchPage> {
       onTap: () {
         setState(() {});
       },
+      onChanged: (value) {
+        if (value.isEmpty) {
+          clearSearch();
+        }
+      },
+    );
+  }
+
+  void clearSearch() {
+    setState(() {
+      _productSearches.clear();
+    });
+  }
+
+
+  Widget productCard(Product product) {
+    return Card(
+      child: ListTile(
+        leading: Image.network(product.imageUrl, width: 50, height: 50),
+        title: Text(product.name),
+        onTap: () {
+          saveSearch(product);
+          // Navigate to product details page or any other action
+        },
+      ),
+    );
+  }
+
+  Widget productSearchCard(Product product) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            //color: Colors.deepPurple[50], // Default background color
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, // Ensure button stays fixed
+            children: [
+              Image.network(product.imageUrl, width: 100, height: 100),
+              SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.name,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      'Price: \₹${product.price.toStringAsFixed(2)}',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: 10),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AddToCartButton(
+                    productName: product.name,
+                    productPrice: product.price.toInt(),
+                    productImage: product.imageUrl,
+                    productUnit: 0, // Set product unit to 0 since it's not used
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 10), // Adjust spacing as needed
+      ],
+    );
+  }
+
+
+
+  Widget recentSearchCard(Product product) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Image.network(product.imageUrl, width: 40, height: 40),
+            // Adjust size here
+            SizedBox(height: 5),
+            // Add padding here
+            Text(product.name),
+          ],
+        ),
+      ),
     );
   }
 
@@ -116,81 +223,96 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Product Search')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            searchBar(),
-            const SizedBox(height: 10),
-            if (_filteredProducts.isNotEmpty)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: _filteredProducts.length,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              searchBar(),
+              const SizedBox(height: 20),
+              if (_filteredProducts.isNotEmpty)
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Search Results',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    const SizedBox(height: 10),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: _filteredProducts.length,
+                      itemBuilder: (context, index) {
+                        final product = _filteredProducts[index];
+                        return productCard(product);
+                      },
+                    ),
+                  ],
+                ),
+              if (_recentSearches.isNotEmpty) ...[
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Recently Searched',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    TextButton(
+                      onPressed: clearRecentSearches,
+                      child: Text('Clear', style: TextStyle(color: Colors.red)),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 5),
+                const Divider(),
+                const SizedBox(height: 5),
+                SizedBox(
+                  height: 120, // Adjust the height as needed
+                  child: ListView.builder(
+                    itemCount: _recentSearches.length,
+                    scrollDirection: Axis.horizontal,
                     itemBuilder: (context, index) {
-                      final product = _filteredProducts[index];
-                      return ListTile(
-                        leading: Image.network(product.imageUrl,
-                            width: 50, height: 50),
-                        title: Text(product.name),
-                        onTap: () {
-                          saveSearch(product);
-                          // Navigator.push(
-                          //   context,
-                          //   MaterialPageRoute(
-                          //     builder: (context) => DemoPage(),
-                          //   ),
-                          // );
-                        },
+                      final recentSearch = _recentSearches[index];
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: recentSearchCard(recentSearch),
                       );
                     },
                   ),
-                ],
-              ),
-            if (_recentSearches.isNotEmpty) ...[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Recently Searched',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  TextButton(
-                    onPressed: clearRecentSearches,
-                    child: const Text('Clear',
-                        style: TextStyle(color: Colors.red)),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 5),
-              const Divider(),
-              const SizedBox(height: 5),
-              SizedBox(
-                height: 150, // Adjust the height as needed
-                child: ListView.builder(
-                  itemCount: _recentSearches.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    final recentSearch = _recentSearches[index];
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 10),
-                      child: Column(
-                        children: [
-                          Image.network(recentSearch.imageUrl,
-                              width: 50, height: 50),
-                          const SizedBox(height: 5),
-                          Text(recentSearch.name),
-                        ],
-                      ),
-                    );
-                  },
                 ),
-              ),
+              ],
+              if (_productSearches.isNotEmpty) ...[
+                // Renamed from _sessionSearches
+                const SizedBox(height: 20),
+                const Text(
+                  'Product Searches', // Renamed from Session Searches
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  height: 200, // Adjust the height as needed
+                  child: ListView.builder(
+                    itemCount: _productSearches.length,
+                    // Renamed from _sessionSearches
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (context, index) {
+                      final productSearch = _productSearches[index]; // Renamed from _sessionSearches
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: productSearchCard(
+                            productSearch), // Renamed from sessionSearchCard
+                      );
+                    },
+                  ),
+                ),
+              ],
             ],
-          ],
+          ),
         ),
       ),
     );
